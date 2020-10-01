8ff792fbe821cc9c7f9a9b5ccbfc217fff111805
James Runtas <71133703+jamesruntas@users.noreply.github.com>
2020-10-01 18:09:05 -0400

    Lab2 Main

--- lab2/main.c
+++ lab2/main.c
@@ -1,34 +1,76 @@
 #include "msp.h"
+//Lab 2 James Runtas 101109175 SYSC3310 Fall 2020
 
-int main(void)
-{
-    volatile uint32_t i;
 
-    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;  // Stop watchdog timer
+int main (void){
+int rgb_val = 0; 	
+int led1_state = 0;
+volatile uint32_t i;
 
-		P1DIR |= BIT0; //Configure P1.0 as Output
-		P1OUT |= BIT0; //Drive Output High for P1.0
-		P2DIR |= BIT0; //Configure P2.0 as Output
-		P2OUT |= BIT0; //Drive Output High for P2.0
-		P2DIR |= BIT1; //Configure P2.1 as Output
-		P2OUT |= BIT1; //Drive Output High for P2.1
-		P2DIR |= BIT2; //Configure P2.2 as Output
-		P2OUT |= BIT2; //Drive Output High for P2.2
+WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;   // disabling watchdog timer
 	
-		P1DIR &= ~BIT1; //Config P1.1 as input (It will be anyways input after reset)
-		P1REN = BIT1; //Enable Pullup/down for P1.1
-		P1OUT = BIT1; //Select Pullup resistor for P1.1
-		P1DIR &= ~BIT4; //Config P1.4 as input (It will be anyways input after reset)
-		P1REN = BIT4; //Enable Pullup/down for P1.4
-		P1OUT = BIT4; //Select Pullup resistor for P1.4
+P1DIR |= (uint8_t) ((1 << 0)); //output 1.0
+P2DIR |=  (uint8_t) ((1 << 0) | (1 << 1) | (1 << 2)); //output 2.0, 2.1, 2.2
+
+P1->SEL0 &= (uint8_t)(~((1<<0) | (1<<1) | (1<<4))); // setting up inputs 1.0, 1.1 and 1.4
+P1->SEL1 &= (uint8_t) (~((1<<0) |(1<<1) | (1<<4)));
+P2->SEL0 &= (uint8_t)(~((1<<0) | (1<<1) |  (1<<2))); // setting up inputs 2.0, 2.1 and 2.2
+P2->SEL1 &= (uint8_t) (~((1<<0) | (1<<1) |  (1<<2)));
+
+P1DIR |=  (uint8_t) ((0 << 1) | (0 << 4)); 
+P1REN |= (uint8_t) ((0 << 1) | (0 << 4));
+P1OUT |=(uint8_t) ((0 << 1) | (0 << 4));
 	
-    while(1)
-    {
-				if( !(P1IN & BIT1) ){ //Evaluates to True for a 'LOW' on P1.1
-					P1OUT ^= BIT0; //Toggle the state of P1.0
-			}
-				if( !(P1IN & BIT4) ){ //Evaluates to True for a 'LOW' on P1.4
-					P2OUT ^= BIT0; //Toggle the state of P2.0
-			}
+	
+
+while(1){
+	while ((P1IN & (uint8_t)(1<<1))&&(P1IN & (uint8_t)(1<<4))){  //debouncing
+	}
+    i = 100;
+
+    while(i > 0){i--;}
+
+    if(!(P1IN & (uint8_t)(1<<1))){               //handle if button 1 was pressed (case 1) 		
+    	P1OUT ^= (uint8_t)(1<<0);                // Toggle P1.0
+	    
+	    
+	    
+	//I still need some way to turn off LED2 which will be coded right here	
+	    
+	    
+	    
+	    
+	    
     }
-}
+    if(!(P1IN & (uint8_t)(1<<1))){  //handle if button 1 was pressed (case 2)	
+    	led_val++;
+	    
+	if (led1_state == 1){  //if led1 is on, we need to turn it off
+		P1OUT ^= (uint8_t)(0<<0); //turn off led1
+		led1_state = 0;
+        }
+						
+	P2->OUT &= (uint8_t) (rgb_val);   //now that led1 is off we can turn led2 on
+	P2->OUT |= (uint8_t) (rgb_val);
+    } 
+		
+    if(!(P1IN & (uint8_t)(1<<4))){ //handle if button 2 was pressed
+			
+	rgb_val++;
+	if (rgb_val > 7){  //if the end of rgb color cycle is reached, reset
+		rgb_val = 1;
+	}
+	    
+	P2->OUT &= (uint8_t) (rgb_val);   //output new color on led2
+	P2->OUT |= (uint8_t) (rgb_val);
+     }
+     else{
+	continue;
+     }
+		
+		
+} //while (1)
+}//int main
+
+
+
